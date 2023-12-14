8f81251864c161f511e7cc1dba06e602040585d9
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-14 11:12:37 +0100

    make so that requirements are executables

---
+++ global_requirements.sh
@@ -0,0 +1,7 @@
+# GLOBAL
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# Install NPM and Node
+nvm install --lts
+# WSL specific dependencies
+sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
---
+++ local_requirement.sh
@@ -0,0 +1,8 @@
+# LOCAL
+# Install resume-cli
+# npm install resume-cli
+# Install puppeteer
+# PUPPETEER_PRODUCT=firefox npm install puppeteer
+
+# Install dependencies from package.json
+PUPPETEER_PRODUCT=firefox npm install
